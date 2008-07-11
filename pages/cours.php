5cc5a9bf28ae61ac10c4b30f1f99d830c9b6ce23
Olivier de Meringo <olivier@demeringo.com>
2008-07-11 17:35:21 +0200

    clean up special chars

--- pages/cours.php
+++ pages/cours.php
@@ -3,7 +3,7 @@
 
 <!-- début du contenu spécifique à la page -->
 <div id="content">	
-<h1 style="text-align:center">Inscriptions - Samba R&eacute;sille<br/>
+<h1 style="text-align:center">Inscriptions - Samba Résille<br/>
 SAISON 2008-2009</h1>
 <table align="center" border="0" cellpadding="2" cellspacing="2">
  <tr><td>
@@ -13,105 +13,98 @@ SAISON 2008-2009</h1>
       <tr align="justify">
       <td colspan="2" rowspan="1" valign="top"><font face="Arial">
       <br/>
-      Samba R&eacute;sille a d&eacute;velopp&eacute; son <b>&eacute;cole associative</b> toujours dans le m&ecirc;me
-but solidaire et citoyen de donner acc&egrave;s &agrave; l'art. Que l'on soit
-virtuose, ou plut&ocirc;t "gauche" de la musique, <b>chacun trouve sa place</b>,
-parce qu'on ne vient pas &agrave; Samba R&eacute;sille pour ressortir diplom&eacute; de
-Samba, on vient y d&eacute;couvrir un patrimoine culturel riche et vari&eacute;,
-d'influences multiethnique tout comme l'est le pays qui l'abrite.<br>
-Les cours d&eacute;butants traversent en musique les principales influences du
-Br&eacute;sil, les cours interm&eacute;diaires quand &agrave; eux, oscilent entre les rytmes
-traditionnels et les cr&eacute;ations et arrangements de la Troupe Samba
-R&eacute;sille.<br>
-      <br>
-      <b>La p&eacute;dagogie</b> a &eacute;t&eacute; invent&eacute;e pour respecter l'accessibilit&eacute;
-&agrave; tous et
+      Samba Résille a développé son <b>école associative</b> toujours dans le même
+but solidaire et citoyen de donner accès à l'art. Que l'on soit
+virtuose, ou plutôt "gauche" de la musique, <b>chacun trouve sa place</b>,
+parce qu'on ne vient pas à Samba Résille pour ressortir diplomé de
+Samba, on vient y découvrir un patrimoine culturel riche et varié,
+d'influences multiethnique tout comme l'est le pays qui l'abrite.<br/>
+Les cours débutants traversent en musique les principales influences du
+Brésil, les cours intermédiaires quand à eux, oscilent entre les rytmes
+traditionnels et les créations et arrangements de la Troupe Samba
+Résille.<br/>
+      <br/>
+      <b>La pédagogie</b> a été inventée pour respecter l'accessibilité
+à tous et
 toutes, elle est, de tradition chez nous, <b>orale et corporelle</b>.
 Explorer
-le rythme, travailler l'ind&eacute;pendance gauche droite, pied main, enrichir
-son patrimoine culturel, travailler la solidarit&eacute; et la compl&eacute;mentarit&eacute;
+le rythme, travailler l'indépendance gauche droite, pied main, enrichir
+son patrimoine culturel, travailler la solidarité et la complémentarité
 des individus et du groupe, sont les objectifs principaux du processus
-d'apprentissage. <b>Ainsi, aucun pr&eacute;-requis musical n'est demand&eacute; &agrave;
+d'apprentissage. <b>Ainsi, aucun pré-requis musical n'est demandé à
 votre
-arriv&eacute;e. </b><br>
-      <br>
-      <i>Le mat&eacute;riel musical est fourni, les cours ont lieu dans le
+arrivée. </b><br/>
+      <br/>
+      <i>Le matériel musical est fourni, les cours ont lieu dans le
 local de
-l&#8217;association 38 rue Roquelaine. Des protections auditives sont
-distribu&eacute;es gratuitement en d&eacute;but d&#8217;ann&eacute;e, mais devront &ecirc;tre achet&eacute;es
-par la suite.</i></font><i> </i><br>
+l'association 38 rue Roquelaine. Des protections auditives sont
+distribuées gratuitement en début d'année, mais devront &ecirc;tre achetées
+par la suite.</i><br/>
       </td>
     </tr>
     <tr>
-      <td colspan="2" rowspan="1" valign="top">
-      <hr size="2" width="100%"></td>
+      <td colspan="2" rowspan="1" valign="top"><hr size="2" width="100%"></td>
     </tr>
     <tr align="center">
-      <td colspan="2" rowspan="1" valign="top"><big><font
- color="#000099"><big><b><font face="Arial">TARIFS</font></b></big></font></big></td>
+      <td colspan="2" rowspan="1" valign="top"><big><font color="#000099"><big><b><font face="Arial">TARIFS</font></b></big></td>
     </tr>
     <tr>
-      <td valign="top"><font face="Arial"><b>Cours adultes d&eacute;butants</b><br>
-Lundi 18h30 - 20h30<br>
-Mardi 20h - 22h<br>
-      <br>
-      <b>Cours adultes interm&eacute;diaires</b><br>
-Lundi 21h - 23h<br>
-      <br>
-      <b>Cours &eacute;veil musical (4 &agrave; 5 ans)</b><br>
-Mercredi 17h - 18h<br>
-      <b>Eveil Batucada (5 &agrave; 7 ans)</b><br>
-Mercredi 16h - 17h<br>
-      <b>Batucada Enfants (8 &agrave; 11 ans)</b><br>
-Mardi 17h30 - 18h30<br>
-      <br>
-      <b>Adh&eacute;sion annuelle </b></font><font face="Arial"><b><i>(obligatoire)</i></b></font><font
- face="Arial"><br>
-Octobre 2008 &agrave; Septembre 2009<br>
-      <br>
-      <i>* 23 euros = demandeur d&#8217;emploi, scolaires, retrait&eacute;s - 31
-euros = salari&eacute;s</i></font> </td>
-      <td valign="top"><font face="Arial"><b>23 ou 31 euros</b>*<br>
-par mois<br>
-      <br>
-      <br>
-      </font><font face="Arial"><b>23 ou 31 euros</b></font><font
- face="Arial">*<br>
-par mois<br>
-      <br>
-      <b>25 euros</b><br>
-par mois<br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <b>8 euros</b></font><br>
+      <td valign="top"><font face="Arial"><b>Cours adultes débutants</b><br/>
+Lundi 18h30 - 20h30<br/>
+Mardi 20h - 22h<br/>
+      <br/>
+      <b>Cours adultes intermédiaires</b><br/>
+Lundi 21h - 23h<br/>
+      <br/>
+      <b>Cours éveil musical (4 à 5 ans)</b><br/>
+Mercredi 17h - 18h<br/>
+      <b>Eveil Batucada (5 à 7 ans)</b><br/>
+Mercredi 16h - 17h<br/>
+      <b>Batucada Enfants (8 à 11 ans)</b><br/>
+Mardi 17h30 - 18h30<br/>
+      <br/>
+      <b>Adhésion annuelle </b><font face="Arial"><b><i>(obligatoire)</i></b></font><font
+ face="Arial"><br/>
+Octobre 2008 à Septembre 2009<br/>
+      <br/>
+      <i>* 23 euros = demandeur d'emploi, scolaires, retraités - 31
+euros = salariés</i></font> </td>
+      <td valign="top"><font face="Arial"><b>23 ou 31 euros</b>*<br/>
+par mois<br/>
+      <br/>
+      <br/>
+      <font face="Arial"><b>23 ou 31 euros</b></font><font face="Arial">*<br/>par mois<br/>
+      <br/>
+      <b>25 euros</b><br/>
+      par mois<br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <b>8 euros</b></font><br/>
       </td>
     </tr>
     <tr>
-      <td colspan="2" rowspan="1" valign="top">
-      <hr size="2" width="100%"></td>
+      <td colspan="2" rowspan="1" valign="top"><hr size="2" width="100%"></td>
     </tr>
     <tr align="center">
-      <td colspan="2" rowspan="1" valign="top"><big><font
- color="#000099" face="Arial"><b><big>COMMENT S'INSCRIRE?</big><br>
-      </b></font></big></td>
+      <td colspan="2" rowspan="1" valign="top"><font color="#000099" face="Arial"><b><big>COMMENT S'INSCRIRE?</big><br/>
+      </b></font></td>
     </tr>
     <tr>
       <td colspan="2" rowspan="1" valign="top"><font face="Arial"><b>Les
-inscriptions d&eacute;butent &agrave; partir du 1er Juillet 2008.</b><br>
-      <br>
-Vous pouvez :<br>
-venir directement au local de l'association, 38 rue Roquelaine Tlse,<br>
-appeller l'association au 05 34 416 216<br>
-ou envoyer un mail &agrave; <a class="moz-txt-link-abbreviated"
- href="mailto:cicc@samba-resille.org">cicc@samba-resille.org</a>.<br>
-      <br>
-      </font>
+inscriptions débutent à partir du 1er Juillet 2008.</b><br/>
+      <br/>
+Vous pouvez :<br/>
+venir directement au local de l'association, 38 rue Roquelaine Tlse,<br/>
+appeller l'association au 05 34 416 216<br/>
+ou envoyer un mail à <a class="moz-txt-link-abbreviated"
+ href="mailto:cicc@samba-resille.org">cicc@samba-resille.org</a>.<br/>
+      <br/>
       <div><font face="Arial">Dans tous les cas,
-pr&eacute;cisez vos nom, pr&eacute;nom, contact t&eacute;l&eacute;phonique, mail ainsi que
-l'atelier dans lequel vous souhaitez vous inscrire!</font><br>
+précisez vos nom, prénom, contact téléphonique, mail ainsi que
+l'atelier dans lequel vous souhaitez vous inscrire!</font><br/>
       </div>
       </td>
     </tr>
