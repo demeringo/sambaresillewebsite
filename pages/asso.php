f2867871ca4ad79488d52fe22f12b0356b15a933
Olivier de Meringo <olivier@demeringo.com>
2008-07-30 17:43:24 +0200

    includes.html replaces the page headers includes.

--- pages/asso.php
+++ pages/asso.php
@@ -1,6 +1,12 @@
-<?php include("basicPageHeader.html"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+	"http://www.w3org/TR/xhtml/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+	<?php include("includes.html"); ?>
+</head>
+<body>
+<div id="page">
 <?php include("headersAndMenu.php"); ?>
-
 <!-- début du contenu spécifique à la page -->
 <div id="content">	
 		<h1>Samba Résille: l'association</h1>
@@ -8,26 +14,20 @@
 		<h2>Qui sommes nous ?</h2>
 		<img class="right"  alt="le CICC" src="../img/cicc/ciccDevanture.jpg"/>	
 		<p>Développée autour de la pratique amateur de la musique brésilienne, l'association Samba Résille s'attache à la rendre accessible aux publics les plus divers.</p>
-				<p>Ce collectif oeuvre dans sa ville, dans son quartier et organise ses actions à partir de sa quadra : le <strong>Centre d'Initiative Culturelle et Citoyenne (CICC)</strong>, une maison de la Samba à la française.</p>
+		<p>Ce collectif oeuvre dans sa ville, dans son quartier et organise ses actions à partir de sa quadra : le <strong>Centre d'Initiative Culturelle et Citoyenne (CICC)</strong>, une maison de la Samba à la française.</p>
 		<p>Vous pouvez télécharger <a href="../documents/Statuts_du_15_avril_2007.pdf" type="application/pdf" >nos statuts</a> (pdf).</p>
 		
 		<h2>Action !</h2>
 		<p>Samba Résille agit pour répondre aux objectifs suivants :</p>
 	<ul>
-		<li>accompagner et développer les <strong>pratiques en amateur</strong> :
-		
-		résidences permanentes de la Troupe Samba Résille et d'une troupe de théâtre de rue, la formule MADJ, mise en place de cours et ateliers de percussions (brésiliennes, orientales, africaines), accueil de musiciens en résidence temporaire et expositions de plasticiens.</li>
-				
+		<li>accompagner et développer les <strong>pratiques en amateur</strong> : résidences permanentes de la Troupe Samba Résille et d'une troupe de théâtre de rue, la formule MADJ, mise en place de cours et ateliers de percussions (brésiliennes, orientales, africaines), accueil de musiciens en résidence temporaire et expositions de plasticiens.</li>
 		<li>développer des actions d'<strong>éducation artistique</strong> et de <strong>médiation culturelle</strong> en direction de tous publics, milieu scolaire et extrascolaire, publics en difficultés sociales, physiques et autres dans un souci d'<strong>accès à la culture pour tous</strong>.</li>
-	
 		<li>proposer des <strong>actions de formation</strong>.</li>
 		<li><strong>travailler en réseau.</strong></li>
 	</ul>
 	<br/>
-		<img class="center" alt="Encontro de Marseille" src="../img/Marseille.jpeg"/>
-	
-				
-		</div>
+	<img class="center" alt="Encontro de Marseille" src="../img/Marseille.jpeg"/>
+	</div>
 <!-- fin du contenu spécifique de la page -->
 <?php include("footer.html"); ?>
 
