503d0579933e6eea27fe71694927379df8a78c84
Olivier de Meringo <olivier@demeringo.com>
2008-08-04 20:48:19 +0200

    nice black style

--- pages/manageAgenda.php
+++ pages/manageAgenda.php
@@ -1,13 +1,13 @@
 <?php
 include "config/auth-config.php";
-include "templates/authcheck.php";
+include "security/authcheck.php";
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
 	"http://www.w3org/TR/xhtml/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <head>
-	<?php include("includes.html"); ?>
+	<?php include("includes-black.html"); ?>
 </head>
 <body>
 <div id="page">
--- pages/privateSpace.php
+++ pages/privateSpace.php
@@ -2,7 +2,7 @@
 	"http://www.w3org/TR/xhtml/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <head>
-	<?php include("includes.html"); ?>
+	<?php include("includes-black.html"); ?>
 </head>
 <body>
 <div id="page">
@@ -12,10 +12,30 @@
 		<h1>Espace privé !</h1>
 
 		<h2>Espace adhérent</h2>
-		<p><a href="http://samba.resille.free.fr/wiki">Accès à l'espace adhérent</a>. Il s'agit d'un espace privé auquel les adhérents peuvent contribuer et il nécessite un mot de passe. Contacter <a href="mailto:webmaster@samba-resille.org">webmaster@samba-resille.org</a> pour plus d'info</a>.
+		<br/>
+		<p>Il s'agit d'un espace privé auquel les adhérents peuvent contribuer et il nécessite un mot de passe. Contacter <a href="mailto:webmaster@samba-resille.org">webmaster@samba-resille.org</a> pour plus d'info</a>.
 		</p>
-		
+		<br/>
+		<p style="text-align:center">
+		<a href="http://samba.resille.free.fr/wiki">Accès à l'espace adhérent (Wiki)</a> 
+		</p>
+		<br/>
+		<br/>
+		<hr/>
+		<br/>
 		<h2>Gestion de l'agenda</h2>
-		<p>Les salariés et administrateurs peuvent <a href="manageAgenda.php">modifier l'agenda des évenements</a>.
+		<p>Les salariés et administrateurs peuvent modifier l'agenda des évenements
 		</p>
-	</div>
+		<br/>
+		<p style="text-align:center">
+		<a href="manageAgenda.php">Accès à la gestion de l'agenda</a>
+		</p>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+    </div>
+	<?php include("footer.html"); ?>
+</body>
+</html>
