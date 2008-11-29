767d5e5621663f65d670dfd2da661f8bdaef7a9f
Olivier de Meringo <olivier@demeringo.com>
2008-11-29 19:13:56 +0100

    da krew link and reordering

--- pages/links.php
+++ pages/links.php
@@ -11,12 +11,14 @@
 <!-- début du contenu spécifique à la page -->
 <div id="content" class="links">	
 		<h1>Liens</h1>
-		<h2>Acteurs culturels locaux</h2>	
+		<h2>Acteurs culturels locaux et partenaires</h2>
+		<p/>	
 		<h3>Le COUAC (COllectif d'Urgence d'Acteurs Culturels de l'agglomération toulousaine)</h3>
 		<p>
 		<a href="http://couac.org" target="_blank"><img class="right" width="150" src="../img/links/couac.jpg" alt="logo du COUAC"/></a>
 		Fondée sur le constat qu'il n'y avait pas dans l'agglomération toulousaine, de lieu de réflexion et de coordination, ni d'outil de proposition en matière de développement de la politique culturelle dans les champs non couverts et pris en charge par l'institution, l'association <a href="http://couac.org" target="_blank">le Couac</a> s'engage sur 4 axes de réflexion et d'action :<br/><br/>- la proximité, l'accessibilité, la démocratie de la culture et de l'acte artistique<br/><br/>- la reconnaissance de la culture en tant que service d'utilité publique et la défense de sa dimension non marchande<br/><br/>- l'ouverture de lieux, de création, de diffusion, de formation et d'échanges<br/><br/>- la redéfinition du rapport des institutions aux acteurs culturels<br/><br/>
 </p>
+<p/>
   <h3>L'association Comunic'arte</h3>
 <p>L'association <a href="http://www.assocomunicarte.com/" target="_blank">Comunic'arte</a> est née d'une volonté de favoriser la communication entre les différents acteurs du Brésil dans le Sud-Ouest.</p>
 <a href="http://www.assocomunicarte.com/" target="_blank">
@@ -29,12 +31,30 @@
 Samba résille est membre de Communic'arte.
 </p>
 <br/>
-  <a href="http://www.mixart-myrys.org/" target="_blank">	<img class="right" width="60" src="../img/links/myrys.jpg" alt="logo de Myrys"/></a>
-
+<p/>
+  	
+  	<img class="right" width="100" src="../img/links/logo_bubblies_pills.gif" alt="logo des bubblies"/>
+		<h3>Les bubblies</h3>
+		<p>	
+		Si vous ne connaissez pas encore <a href="http://www.bubblies.net/" target="_blank">les bubblies</a>, allez découvrir ce groupe qui montre à tous que l'indépendance peut rimer avec projets d'envergure.
+		</p>
+		
+		<p/>
+		<img class="right" width="75" src="../img/links/daKrew_small.jpg" alt="logo da Krew"/>
+		<h3>Da Krew</h3>
+		<p>
+		Parti d'une base Hip-Hop, le Da Krew expérimente et s'inspire de différents courants musicaux pour créer une alchimie entre performance lyrique et instruments électroniques.<br/>
+		Partenaires de samba résille sur des résidendes et soirées concert, vous pouvez les écouter sur <a href="http://www.da-krew.net/" target="_blank">le site du Da Krew</a> et leur <a href="http://www.myspace.com/dakrewlegroupe" target="_blank">Myspace</a>.
+		</p>
+		
+		<p/>
+		<a href="http://www.mixart-myrys.org/" target="_blank">	<img class="right" width="60" src="../img/links/myrys.jpg" alt="logo de Myrys"/></a>
 		<h3>Mix'Art Myrys</h3>
 			<p>
 		Mix'Art Myrys est un bouillonant collectif d'artistes autogéré. Concerts, performaces artistiques, expos ... ils savent tout faire et le meilleur moyen de se faire une idée est d'aller leur <a href="http://www.mixart-myrys.org/" target="_blank">rendre visite</a> sur leur site.	
 		</p>						
+
+    <p/>
 		<h3>Le Rém-i (Réseau des musiciens indépendants)</h3>
 		<a href="http://www.rem-i.org/" target="_blank">
 		<img class="right" width="50" src="../img/links/Remi-logo.jpg" alt="logo du Rem-i"/>
@@ -44,15 +64,9 @@ Samba résille est membre de Communic'arte.
 		<p>
 		 La plupart de groupes suivis par l'association sont librement disponibles en téléchargement sur le site et il y a de très bonnes choses !
 		 </p>
-		<img class="right" width="100" src="../img/links/logo_bubblies_pills.gif" alt="logo des bubblies"/>
-		<h3>Les bubblies</h3>
-		<p>	
-		Si vous ne connaissez pas encore <a href="http://www.bubblies.net/" target="_blank">les bubblies</a>, allez découvrir ce groupe qui montre à tous que l'indépendance peut rimer avec projets d'envergure.
-		</p>
-		
-		
 
-		
+
+		<p/>
 		<h2>Partitions, paroles et techniques instrumentales</h2>
 		<h3>Musique Populaire Brésilienne</h3>
 		<p>
@@ -61,9 +75,12 @@ Samba résille est membre de Communic'arte.
 		- Un autre très bon site: <a href="http://cifraclub.terra.com.br/">cifraclub</a> avec de nombreuses partitions et paroles (les accords sont indiqués dans le texte des chansons).
 		</p>
 		
+		<p/>
 		<h3>Les cours d'instruments de K-nard :</h3>
 		<p>Les cours en vidéo pour 
 <a href="http://www.dailymotion.com/video/xmlqe_jouer-le-tambourim-avec-knard" target="_blank">le tambourim</a> et <a href="http://www.dailymotion.com/video/xo9ta_tantan-tourne" target="_blank">le tan-tan</a></p>
+		
+		<p/>
 		<h3>Les cahiers de Bando do Chorão</h3>
 		<p>Des grilles harmoniques à foison pour encourager les rodas de Choro:<br/>
 		<a href=" http://bando.do.chorao.free.fr/root/caderno/caderno-harmonias-vol1.htm" target="_blank">Volume 1 (pdf) </a> et <a href="http://bando.do.chorao.free.fr/root/caderno/caderno-harmonias-vol2.htm" target="_blank">Volume 2 (pdf)</a>
