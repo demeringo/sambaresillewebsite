1cde6c9ce2af7d82da47c6331a7fac864d27b91e
Olivier de Meringo <olivier@demeringo.com>
2009-01-19 14:20:52 +0100

    relooking contacts

--- pages/contacts.php
+++ pages/contacts.php
@@ -12,42 +12,39 @@
 <div id="content">	
 		<h1>Contacts</h1>
 	
-
+<!-- Lien vers google map -->
+				<a href="http://maps.google.fr/maps?f=q&hl=fr&geocode=&q=38+rue+roquelaine+toulouse&sll=47.15984,2.988281&sspn=14.527976,27.905273&ie=UTF8&layer=c&g=38+rue+roquelaine+toulouse&ll=43.636572,1.42067&spn=0.123988,0.263672&z=12&iwloc=r2"><img class="right" style="border: 1px solid red" title="plan d'acces google map" src="../img/contact/google_map_mini.png" alt="plan d'acces google map"/></a>
 		<h2>Pour toute info</h2>
 		<!--<a href="../img/contact/planAcces.jpg"><img class="right" src="../img/contact/planMiniature.jpg" alt="plan d'acces"/></a>	-->
-			<!-- Lien vers google map -->
-				<a href="http://maps.google.fr/maps?f=q&hl=fr&geocode=&q=38+rue+roquelaine+toulouse&sll=47.15984,2.988281&sspn=14.527976,27.905273&ie=UTF8&layer=c&g=38+rue+roquelaine+toulouse&ll=43.636572,1.42067&spn=0.123988,0.263672&z=12&iwloc=r2"><img class="right" style="border: none" title="plan d'acces google map" src="../img/contact/google_map_mini.png" alt="plan d'acces google map"/></a>
-					<ul>
-			<li>e-mail:  <a href="mailto:cicc@samba-resille.org" >CICC@samba-resille.org</a></li>
-			<li>tel: +33 (0) 534 416 216</li>
-			<li>fax: +33(0) 534 416 100</li>
-
+								<ul>
 			<li>Samba Résille, 38 rue Roquelaine, 31000 Toulouse.</li>
 			<li><strong>Métro Jeanne d'Arc.</strong></li>
-			<li>Pour le site web : <a href="mailto:webmaster@samba-resille.org">webmaster@samba-resille.org</a></li>
+			<li>tel: +33 (0) 534 416 216</li>
+			<li>fax: +33(0) 534 416 100</li>
+			<li>e-mail:  <a href="mailto:cicc@samba-resille.org" >CICC@samba-resille.org</a></li>
 		</ul>	
 		<!-- </div>-->
 
 		<h2>L'équipe</h2>
-		<p><strong>Le Bureau de l'association</strong></p>
-		<ul>
-			<li>Président et vice président:<br/>  David Crochet  / Olivier de Méringo</li>
-			<li>Trésorière:<br/> Sandie Guilloteau</li>
-			<li>Secrétaire et adjointe: Géraud Arnal / Florence Bloch </li>
-		</ul>
-		<p><strong>Le conseil d'admnistration</strong></p>
-		<p>Outre les membres du bureau, le conseil d'administration est constitué de:<br/> Stéphane Robaire, Virginie Grayo, Nanou (Anne) Radigalès,
-		Alexandre Nouvel, Anne sophie Lacamp, Marie Cabane, Benoit Cornille, Didier Darre, Marie Emilie Rouffet.<br/>
-		Vous pouvez joindre l'équipe du CA à  <a href="mailto:ca-samba-resille@yahoogroupes.fr">ca-samba-resille@yahoogroupes.fr</a>
-		</p>
-		 <p><strong>L'équipe salariée</strong></p>
+		<p><strong>L'équipe salariée</strong></p>
 		<ul id="equipe">
 			<li>Hamza Medkouri: Directeur</li>
 			<li>Audrey Campourcy: Coordination Pratiques Artistiques et Evènements du CICC</li>
 			<li>Gregory Valter: Coordination Education Artistique et Médiation Culturelle</li>
 			<li>Johan Andoche: Intervenant Musical</li> 
 		</ul> 
-		<h2>Infos légales</h2>
+		<p><strong>Le Bureau de l'association</strong></p>
+		<ul>
+			<li>Président et vice président: David Crochet  / Olivier de Méringo</li>
+			<li>Trésorière: Sandie Guilloteau</li>
+			<li>Secrétaire et adjointe: Géraud Arnal / Florence Bloch </li>
+		</ul>
+		<p><strong>Le conseil d'admnistration</strong></p>
+		<p>Outre les membres du bureau, le conseil d'administration est constitué de:<br/>Virginie Grayo, Anne Radigalès,
+		Alexandre Nouvel, Anne sophie Lacamp, Marie Cabane, Benoit Cornille, Didier Darre, Marie Emilie Rouffet.<br/>
+		Vous pouvez joindre l'équipe du CA à  <a href="mailto:ca-samba-resille@yahoogroupes.fr">ca-samba-resille@yahoogroupes.fr</a>
+		</p>
+		 		<h2>Infos légales</h2>
 	  <p>Association loi 1901</p>
 	  <ul>
         <li>Agréée Jeunesse et Education Populaire n° 31-580</li>
