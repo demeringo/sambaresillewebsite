050525f0265cec09b0505f072a49827dd739affb
Olivier de Meringo <olivier@demeringo.com>
2009-03-05 13:53:09 +0100

    Adjust blackout banner width

--- blackout/blackout.js
+++ blackout/blackout.js
@@ -6,7 +6,7 @@ $("img,a,input,div,p,h1,h2,h3,h4,class,element.style").animate({ opacity: 0.95 }
 //$("ul,li,a,div,p,h1,h2,h3,h4,class,element.style").css("color", "white");
 $(".hadopi").css("border", "2");
 $(".hadopi").css("background", "2");
-$("body > *:first").before("<div style='margin: auto; margin-top: 5px; text-align:center; width: 80%; border: 2px solid white; height: 250px;'><iframe style='border: 0px;' height=100% width=100% src='/publicSite/blackout/message.html'></iframe></div>");
+$("body > *:first").before("<div style='margin: auto; margin-top: 5px; text-align:center; width: 800px; border: 1px solid white; height: 250px;'><iframe style='border: 0px;' height=100% width=100% src='/publicSite/blackout/message.html'></iframe></div>");
 });
 
 
