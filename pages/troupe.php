897bef103ffbaa20569e1a7cd52478829ddd91e2
olivier <olivier@whitecat.lan>
2009-08-23 21:46:05 +0200

    video, new text and new picts

--- pages/troupe.php
+++ pages/troupe.php
@@ -9,22 +9,23 @@
 <?php include("headersAndMenu.php"); ?><!-- début du contenu spécifique à la page -->
 	<div id="content">	
 		<h1>Samba Résille : La troupe</h1>
-		
-		<h2>Batucada Si !</h2>	
-		<img class="center" width="450" src="../img/troupe/small_troupe1.jpg" alt="Photo de la troupe"/>
+		<!-- Following div links to the daily motion video of samba resille -->
+		<div><object width="480" height="414" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"><param name="movie" value="http://www.dailymotion.com/swf/xa5jfk_samba-resille_creation"></param><param name="allowFullScreen" value="true"></param><param name="allowScriptAccess" value="always"></param><embed src="http://www.dailymotion.com/swf/xa5jfk_samba-resille_creation" type="application/x-shockwave-flash" width="480" height="414" allowFullScreen="true" allowScriptAccess="always"></embed></object></div>
+		<h2>La rue Comme scène favorite !</h2>
 		<p>
-		Composée d'une trentaine de percussionnistes, la troupe propose des animations de rue et des spectacles sur scène bourrés d'énergie.</p>
+		Samba Résille revisite à sa manière, les rythmes du Brésil, avec la même ferveur et toujours plus de joie, pour faire danser petits et grands, autour d'elle, en France, en Europe et en Afrique du Nord.</p>
 		<p>
-		Sur des rythmes principalement issus de la tradition populaire du Brésil (samba batucada, samba reggae, samba funk, baião...), le public se voit emporté dans la frénésie de la batterie et la joie de bouger en cadence.
-		</p>
-		<img class="right" width="240" src="../img/troupe/small_troupe2.jpg" alt="photo troupe"/>
-		<img class="right" width="240" src="../img/troupe/small_troupe3.jpg" alt="photo troupe"/>
-		
-
-		
-						
-
-	</div>
+Depuis 1992, composée d'une trentaine de percussionnistes, la troupe propose des animations et spectacles de rue et sur scène bourrés d'énergie.
+Sur des rythmes <strong>principalement issus de la tradition populaire du Brésil</strong> (enredo, reggae, baião, merengue, ...), l'appropriation artistique de <strong>ce patrimoine se mixe aujourd'hui aux influences d'Europe du Sud des Antilles et du Maghreb, sur des créations originales</strong> funk, ragga/hip-hop, drum'n bass...</p>
+<p>
+En somme, presque un tour du monde rythmique, notre premier patrimmoine commun.
+</p>
+<br/>
+<br/>
+<img class="right" width="160" src="../img/troupe/Troupe_CARNAVAL_thumb.jpg" alt="carnaval"/>
+<img class="right" width="190" src="../img/troupe/Troupe_MARATHON_thumb.jpg" alt="marathon de toulouse"/>
+<img class="right" width="170" src="../img/troupe/Troupe_VIC_thumb.jpg" alt="férias de VIC"/>
+</div>
 <!-- fin du contenu spécifique de la page -->
 <?php include("footer.html"); ?>
 
