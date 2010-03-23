e70f01784d3a96efd6f200426ee4bc387036d4cf
olivier <olivier@whitecat.lan>
2010-03-23 23:11:47 +0100

    expo icepack sonar

---
+++ pages/stages-expos/icepack-sonar.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3org/TR/xhtml/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+	<?php include("../includes-black.html"); ?>
+</head>
+<body>
+<div id="page">
+<?php include("../headersAndMenu.php"); ?>
+<!-- début du contenu spécifique à la page -->
+<div id="content">
+<div class="page-title">
+	<h1>Exposition</h1>
+</div>
+
+
+<h1 style="text-align:center">MO LO "In Sectes"</h1>
+<h2 style="text-align:center">Peintre, dessinateur toulousain</h2>
+<h2 style="text-align:center">Jusqu'au 10 novembre</h2>
+
+
+<p style="text-align:center">
+Habitué de Samba Résille, Mo Lo nous avait caché son talent de dessinateur. <br/>
+Il dévoile enfin son univers, "In Sectes", une expo riche et vivante, qui fait déjà sensation dans nos murs.
+</p>
+<br/>
+<br/>
+<br/>
+<img class="center" width="454px" src="../../img/expo/mo-lo-insectes.gif" alt="Flyer du vernissage"/>
+<br/>
+<br/>
+<br/>
+<h3 style="text-align:center">
+Le vernissage aura lieu en compagnie des Figues de Barbarie (Rythm'n Blues Bollywoodien) le 26 octobre
+</h3>
+
+
+
+
+</div>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+
+
+
+
+</div>
+<!-- fin du contenu specifique de la page -->
+<?php include("../footer.html"); ?>
+</body>
+</html>
+
