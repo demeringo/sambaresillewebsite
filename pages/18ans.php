9ca748d64524e9cf0c712d6e1bc6c516c6c44604
olivier <olivier@whitecat.home>
2010-05-24 23:16:30 +0200

    Page 18 ans et pop up sur home page (ajout sur asso.php).

---
+++ pages/18ans.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+	"http://www.w3org/TR/xhtml/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+	<?php include("includes-black.html"); ?>
+	<link rel="stylesheet" href="/publicSite/basic-style/styles-black.css" type="text/css"/>
+</head>
+<body>
+<div id="page">
+<?php include("headersAndMenu.php"); ?>
+<!-- debut du contenu specifique a la page -->
+<div id="content">	
+
+
+
+
+<br/>
+<br/>
+<img class="center" width="500" src="../img/18ans/Title2ORANGE.gif" alt="Samba Résille fête ses 18 ans"/>
+
+
+
+
+
+<br/>
+<br/>
+<br/>
+<h1 style="text-align:center">Du 29 mai au 9 juillet</h1>
+<br/>
+
+<h2 style="text-align:center">Parades, concerts, expos, forums et activités jeune public</h2>
+
+<br/>
+<img class="right" width="180" src="../img/18ans/Theme.gif" alt="Racines d'ailleurs, rites d'aujourd'hui"/>
+<br/>
+<br/>
+<p style="text-align:center">Voir <a href="../documents/18_Ans_Samba-Resille_Programmation.pdf">la programmation</a></p>
+<br/>
+<p style="text-align:center">Télécharger <a href="../documents/18_ans_Samba-Resille_Dossier_de _presse.pdf">le dossier de presse</a></p>
+
+
+<br/>
+<br/>
+<br/>
+<img class="left" width="350" src="../img/18ans/FishesBLACK.gif"/>
+<img width="120" src="../img/18ans/OctopussyBLACK.gif"/>
+
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<img class="center" width="300" src="../img/18ans/INSTITS-NOIR.gif"/>
+<img class="center" width="300" src="../img/18ans/logo_fal.gif"/>
+</div>
+<!-- fin du contenu specifique de la page -->
+<?php include("footer.html"); ?>
+</body>
+</html>
+
+
+
--- pages/asso.php
+++ pages/asso.php
@@ -1,14 +1,39 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
-	"http://www.w3org/TR/xhtml/DTD/xhtml1-strict.dtd">
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <head>
 	<?php include("includes.html"); ?>
+	<!-- Popup START -->
+	<link rel="stylesheet" href="/publicSite/basic-style/popup.css" type="text/css" />
+	<script src="http://code.jquery.com/jquery-1.4.2.min.js" type="text/javascript"></script>  
+	<script src="../javascript/popup.js" type="text/javascript"></script>  
+	<!-- Popup END -->
 </head>
 <body>
+
+<div id="backgroundPopup" ></div>
+<!-- Ajout de la pop up de l'anniversaire -->
+<div id="popup">   
+	<h1>Du 29 mai au 7 juillet</h1>
+	<br/><br/>
+	<img width="150" src="../img/18ans/Event.gif" alt="Samba Résille fête ses 18 ans"/>
+	<img width="220" src="../img/18ans/Theme.gif" alt="Racines d'alleurs"/>    
+    <br/><br/><br/><br/><br/><br/><br/><br/>
+    <table>
+    <tr>
+    <td><a href="18ans.php">Programme complet</a></td><td><a id="popupClose">Accès au site</a></td>
+    </tr>
+    </table> 
+</div>  
+<!-- Fin de la pop-up anniversaire -->
+
+
 <div id="page">
 <?php include("headersAndMenu.php"); ?>
-<!-- début du contenu spécifique à la page -->
+
+<!-- Content div start -->
 <div id="content">	
+
 		<h1>Samba Résille: l'association</h1>
 		
 		<h2>Qui sommes nous ?</h2>
@@ -27,8 +52,10 @@
 	</ul>
 	<br/>
 	<img class="center" alt="Encontro de Marseille" src="../img/Marseille.jpeg"/>
-	</div>
+
+</div>
 <!-- fin du contenu spécifique de la page -->
+
 <?php include("footer.html"); ?>
 
 
